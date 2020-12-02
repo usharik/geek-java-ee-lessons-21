f302d15f41972a47491df436005617f052f8334b
usharik <usharik@mail.ru>
2020-12-02 18:27:21 +0100

    Lesson 6 EJB homework

--- jsf-web-app/src/main/java/ru/geekbrains/controller/CartController.java
+++ jsf-web-app/src/main/java/ru/geekbrains/controller/CartController.java
@@ -7,6 +7,7 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @SessionScoped
@@ -15,9 +16,15 @@ public class CartController implements Serializable {
     @EJB
     private CartService cartService;
 
-    // TODO дописать контроллер и создать представление для корзины
-
     public void add(ToDoRepr todo) {
+        cartService.add(todo);
+    }
 
+    public List<ToDoRepr> getAllItems() {
+        return cartService.getAll();
+    }
+
+    public void delete(ToDoRepr toDoRepr) {
+        cartService.delete(toDoRepr.getId());
     }
 }
--- jsf-web-app/src/main/java/ru/geekbrains/service/CartService.java
+++ jsf-web-app/src/main/java/ru/geekbrains/service/CartService.java
@@ -2,17 +2,25 @@ package ru.geekbrains.service;
 
 import javax.ejb.Stateful;
 import java.io.Serializable;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Stateful
 public class CartService implements Serializable {
 
+    private final Map<Long, ToDoRepr> toDoReprMap = new HashMap<>();
+
     public List<ToDoRepr> getAll() {
-        return Collections.emptyList();
+        return new ArrayList<>(toDoReprMap.values());
     }
 
     public void add(ToDoRepr todo) {
+        toDoReprMap.put(todo.getId(), todo);
+    }
 
+    public void delete(Long id) {
+        toDoReprMap.remove(id);
     }
 }
